19a1e9f706c3110705874def95f18c5306d75b55
Kha <k.tran@liqd.net>
2022-03-07 16:13:56 +0000

    cleaning and deleting unused declarations and adding prettier and eslint

--- app/routes/activity-serializer.ts
+++ app/routes/activity-serializer.ts
@@ -1,27 +1,31 @@
 interface TMessage {
-  created_at: string,
-  diff_days: number,
-  platform: string,
-  messages: string[] | [{
-    [key: string]: any
-  }],
-  html_url?: string,
-  coding_lang?: any,
-  action?: string,
-  messagetype?: string,
-  messagelength?: number,
+  created_at: string;
+  diff_days: number;
+  platform: string;
+  messages:
+    | string[]
+    | [
+        {
+          [key: string]: any;
+        }
+      ];
+  html_url?: string;
+  coding_lang?: any;
+  action?: string;
+  messagetype?: string;
+  messagelength?: number;
 }
 
 const calcDiffDays = (createdDate: string) => {
   const now = Date.now();
   const cdate = Date.parse(createdDate);
   return (now - cdate) / 1000 / 60 / 60 / 24;
-}
+};
 
 export const activitySerializer = (
-  messages: string[] = ['Unknown activity.'],
-  platform: string = 'UNKNOWN',
-  created_at: string = 'n/a',
+  messages: string[] = ["Unknown activity."],
+  platform = "UNKNOWN",
+  created_at = "n/a",
   html_url?: string,
   coding_lang?: any,
   action?: string,
@@ -35,7 +39,6 @@ export const activitySerializer = (
     coding_lang,
     diff_days: calcDiffDays(created_at),
     action,
-    messagetype
-  }
-}
-
+    messagetype,
+  };
+};
--- app/routes/activity.tsx
+++ app/routes/activity.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
+import React, { useState } from "react";
 
 const getDays = (diffDays: number) => {
   if (diffDays < 1) {
-    return 'Today';
-  }
-  else if (diffDays < 2) {
-    return 'Yesterday';
-  }
-  else {
+    return "Today";
+  } else if (diffDays < 2) {
+    return "Yesterday";
+  } else {
     return `${Math.round(diffDays)} days ago`;
   }
-}
+};
 
 export const Activity = (props: any) => {
   const { data } = props;
@@ -18,25 +16,23 @@ export const Activity = (props: any) => {
 
   const hasMultiple = data.messages.length > 1;
   const hasLink = data.html_url;
-  const platform = data.platform === 'GITHUB.COM'
-    ? 'github'
-    : 'twitter';
+  const platform = data.platform === "GITHUB.COM" ? "github" : "twitter";
 
   const prevCommitButton = () => {
     return setCurrCommit(currCommit - 1);
-  }
+  };
 
   const nextCommitButton = () => {
     return setCurrCommit(currCommit + 1);
-  }
+  };
 
   const codingLangBadge = () => {
     return (
       <span className="activity-card-info__coding-lang">
         {data.coding_lang}
       </span>
-    )
-  }
+    );
+  };
 
   return (
     <div className="activity-card">
@@ -44,42 +40,44 @@ export const Activity = (props: any) => {
       <div className="activity-card-content">
         <div className="activity-card-info">
           <div className={`activity-card-info__header--${platform}`}>
-            {data.platform}&nbsp;
-            [{data.action}] {data.coding_lang && codingLangBadge()}
+            {data.platform}&nbsp; [{data.action}]{" "}
+            {data.coding_lang && codingLangBadge()}
           </div>
           <div className="activity-card-info__messagetype">
-            {data.messagetype} 
+            {data.messagetype}
           </div>
           <div className="activity-card-info__message">
-            "{data.messages[currCommit]}"
+            &quot;{data.messages[currCommit]}&quot;
           </div>
           <div className="activity-card-info__actions">
-            {hasLink && <div className="activity-card-info__link">
-              <a href={data.html_url} target="_blank">
-                Bring me there -&gt;
-              </a>
-            </div>}
-            {hasMultiple && <div className="activity-card-info__crumb">
-              <button
-                className="button--reset activity-card-commit__button"
-                onClick={prevCommitButton}
-              >
-                &lt;&lt;
-              </button>
-              {`${currCommit + 1}/${data.messages.length}`}
-              <button
-                className="button--reset activity-card-commit__button"
-                onClick={nextCommitButton}
-              >
-                &gt;&gt;
-              </button>
-            </div>}
+            {hasLink && (
+              <div className="activity-card-info__link">
+                <a href={data.html_url} target="_blank" rel="noreferrer">
+                  Bring me there -&gt;
+                </a>
+              </div>
+            )}
+            {hasMultiple && (
+              <div className="activity-card-info__crumb">
+                <button
+                  className="button--reset activity-card-commit__button"
+                  onClick={prevCommitButton}
+                >
+                  &lt;&lt;
+                </button>
+                {`${currCommit + 1}/${data.messages.length}`}
+                <button
+                  className="button--reset activity-card-commit__button"
+                  onClick={nextCommitButton}
+                >
+                  &gt;&gt;
+                </button>
+              </div>
+            )}
           </div>
         </div>
-        <div className="activity-card-date">
-          {getDays(data.diff_days)}
-        </div>
+        <div className="activity-card-date">{getDays(data.diff_days)}</div>
       </div>
     </div>
-  )
+  );
 };
