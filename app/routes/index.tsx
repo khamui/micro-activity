22591c0b4332df7bae1d1f50895b910fef56b30c
Kha <k.tran@liqd.net>
2022-11-07 12:50:56 +0000

    adding additionally most outer component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "remix";
 import { Activity } from "./activity";
 import { ActivitiesService } from "../services/ActivitiesService"
 import {
+  SSRProvider,
   ThemeProvider,
   BaseStyles,
   Box,
@@ -36,78 +37,80 @@ export default function Index() {
   const activities = useLoaderData();
 
   return (
-    <ThemeProvider
-      nightScheme="dark_dimmed"
-    >
-      <BaseStyles>
-        <PageLayout sx={{bg: 'canvas.default'}}>
-          <PageLayout.Header>
-            <Box
-              display="flex"
-              justifyContent="flex-end"
-              px={3}
-              py={2}
-            >
-              <PointerBox
-                caret="right"
-                sx={{
-                  p: 3,
-                  m: 2,
-                  bg: 'done.subtle',
-                  color: 'done.fg',
-                  borderWidth: 1,
-                  borderColor: 'done.fg',
-                  borderStyle: 'solid',
-                  width: '30%'
-                }}
+    <SSRProvider>
+      <ThemeProvider
+        nightScheme="dark_dimmed"
+      >
+        <BaseStyles>
+          <PageLayout sx={{bg: 'canvas.default'}}>
+            <PageLayout.Header>
+              <Box
+                display="flex"
+                justifyContent="flex-end"
+                px={3}
+                py={2}
               >
-                Switch theme between bright and dark.
-              </PointerBox>
-              <SwitchButton />
-            </Box>
-            <Box p={3}>
-              <Heading
-                as="h1"
+                <PointerBox
+                  caret="right"
+                  sx={{
+                    p: 3,
+                    m: 2,
+                    bg: 'done.subtle',
+                    color: 'done.fg',
+                    borderWidth: 1,
+                    borderColor: 'done.fg',
+                    borderStyle: 'solid',
+                    width: '30%'
+                  }}
+                >
+                  Switch theme between bright and dark.
+                </PointerBox>
+                <SwitchButton />
+              </Box>
+              <Box p={3}>
+                <Heading
+                  as="h1"
+                >
+                  Kha`s code contributions. And tweets.
+                </Heading>
+              </Box>
+          </PageLayout.Header>
+            <PageLayout.Content>
+              <Box
+                p={3}
               >
-                Kha`s code contributions. And tweets.
-              </Heading>
-            </Box>
-        </PageLayout.Header>
-          <PageLayout.Content>
-            <Box
-              p={3}
-            >
-              {activities.map((activity: any, idx: number) => (
-                <Activity key={`activity_${idx}`} data={activity} />
-              ))}
-            </Box>
-          </PageLayout.Content>
-          <PageLayout.Footer>
-            <Box as="p" px={3}>
-              Built with{" "}
-              <Link
-                href="https://remix.run/"
-                target="_blank"
-              >
-                Remix
-              </Link>
-              {" "}and{" "}
-              <Link
-                href="https://primer.style/"
-                target="_blank"
-              >
-                Primer
-              </Link>. Available under MIT License on{" "}
-              <Link
-                href="https://github.com/khamui/micro-activity"
-                target="_blank"
-              >
-                GitHub
-              </Link>.
-            </Box>
-          </PageLayout.Footer>
-        </PageLayout>
-      </BaseStyles>
-    </ThemeProvider>
+                {activities.map((activity: any, idx: number) => (
+                  <Activity key={`activity_${idx}`} data={activity} />
+                ))}
+              </Box>
+            </PageLayout.Content>
+            <PageLayout.Footer>
+              <Box as="p" px={3}>
+                Built with{" "}
+                <Link
+                  href="https://remix.run/"
+                  target="_blank"
+                >
+                  Remix
+                </Link>
+                {" "}and{" "}
+                <Link
+                  href="https://primer.style/"
+                  target="_blank"
+                >
+                  Primer
+                </Link>. Available under MIT License on{" "}
+                <Link
+                  href="https://github.com/khamui/micro-activity"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>.
+              </Box>
+            </PageLayout.Footer>
+          </PageLayout>
+        </BaseStyles>
+      </ThemeProvider>
+    </SSRProvider>
   );
 }
