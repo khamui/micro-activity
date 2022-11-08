e4a4195d96a40323716ebf27f70eeb3d2a1870c4
Kha <k.tran@liqd.net>
2022-11-08 11:58:10 +0000

    wrapping most outer scope

--- app/root.tsx
+++ app/root.tsx
@@ -28,21 +28,21 @@ export const meta: MetaFunction = () => {
 
 export default function App() {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <SSRProvider>
+    <SSRProvider>
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+        <body>
           <Outlet />
-        </SSRProvider>
-        <ScrollRestoration />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
-    </html>
+          <ScrollRestoration />
+          <Scripts />
+          {process.env.NODE_ENV === "development" && <LiveReload />}
+        </body>
+      </html>
+    </SSRProvider>
   );
 }
