09a564bcfd553486d8749309ff3c81aaebb94dcc
Kha <k.tran@liqd.net>
2022-11-08 13:09:05 +0000

    Revert "hydrate and ssr: changing entry files"
    This reverts commit 649d3c26c4a3197815d259aac58bb0d9f5b3d9d0.

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { renderToString } from "react-dom/server";
 import { RemixServer } from "remix";
 import type { EntryContext } from "remix";
-import { ServerStyleSheet } from "styled-components";
 
 export default function handleRequest(
   request: Request,
@@ -10,27 +8,14 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  const markup = renderToString(
+    <RemixServer context={remixContext} url={request.url} />
+  );
 
-  const sheet = new ServerStyleSheet();
-  try {
-    const markup = renderToString(
-      sheet.collectStyles(
-        <RemixServer context={remixContext} url={request.url} />
-      )
-    );
-    // const styleTags = sheet.getStyleTags(); // or sheet.getStyleElement();
-    // console.log('STYLE TAGS FROM SERVER', styleTags);
-    responseHeaders.set("Content-Type", "text/html");
-    return new Response("<!DOCTYPE html>" + markup, {
-      status: responseStatusCode,
-      headers: responseHeaders
-    });
-  }
-  catch (error) {
-    // handle error
-    console.error(error);
-  } 
-  finally {
-    sheet.seal();
-  }
+  responseHeaders.set("Content-Type", "text/html");
+
+  return new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders
+  });
 }
