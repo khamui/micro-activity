6c564ef4843edef85a81809439f99a3832747032
Kha <k.tran@liqd.net>
2022-11-08 12:32:35 +0000

    hydrate and ssr: changing entry files

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import { renderToString } from "react-dom/server";
 import { RemixServer } from "remix";
 import type { EntryContext } from "remix";
+import { ServerStyleSheet } from "styled-components";
 
 export default function handleRequest(
   request: Request,
@@ -8,14 +10,27 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
 
-  responseHeaders.set("Content-Type", "text/html");
-
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders
-  });
+  const sheet = new ServerStyleSheet();
+  try {
+    const markup = renderToString(
+      sheet.collectStyles(
+        <RemixServer context={remixContext} url={request.url} />
+      )
+    );
+    // const styleTags = sheet.getStyleTags(); // or sheet.getStyleElement();
+    // console.log('STYLE TAGS FROM SERVER', styleTags);
+    responseHeaders.set("Content-Type", "text/html");
+    return new Response("<!DOCTYPE html>" + markup, {
+      status: responseStatusCode,
+      headers: responseHeaders
+    });
+  }
+  catch (error) {
+    // handle error
+    console.error(error);
+  } 
+  finally {
+    sheet.seal();
+  }
 }
